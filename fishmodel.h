2164682f0cec05214948ee72f4dcfb42c05f9239
Gavin Pease <gavinpease@gmail.com>
2021-12-07 14:15:28 -0700

    Space2Tab

--- fishmodel.h
+++ fishmodel.h
@@ -12,41 +12,41 @@
 
 class FishModel : public QObject {
 
-    Q_OBJECT
+	Q_OBJECT
 
-    public:
-        enum SCENE_STATE {
-            WATER_CHANGE, FILTER_CHANGE, FEEDING, ADD_FISH, PREPARE_TANK
-        };
-    private:
-        std::vector<GameObject *> gameObjects;
-        std::map<std::string, GameObject *> gameObjectMap;
-        float deltaTime;
-        b2World physicsWorld;
+	public:
+		enum SCENE_STATE {
+			WATER_CHANGE, FILTER_CHANGE, FEEDING, ADD_FISH, PREPARE_TANK
+		};
+	private:
+		std::vector<GameObject *> gameObjects;
+		std::map<std::string, GameObject *> gameObjectMap;
+		float deltaTime;
+		b2World physicsWorld;
 
-        void prepareStartUp();
-        void addGameObjectToScene(GameObject *toAdd);
+		void prepareStartUp();
+		void addGameObjectToScene(GameObject *toAdd);
 		void addBodyToWorld(PhysicsGameObject *toAdd);
 
-        bool debug = true;
+		bool debug = true;
 
-        QImage getColliderShape(b2Shape *shape, QColor penColor, QPointF &translation);
-        // The current scene to draw
-        SCENE_STATE currentScene = WATER_CHANGE;
-        void setScene(FishModel::SCENE_STATE scene);
+		QImage getColliderShape(b2Shape *shape, QColor penColor, QPointF &translation);
+		// The current scene to draw
+		SCENE_STATE currentScene = WATER_CHANGE;
+		void setScene(FishModel::SCENE_STATE scene);
 
-    public:
-        FishModel(float deltaTime);
-        ~FishModel();
+	public:
+		FishModel(float deltaTime);
+		~FishModel();
 
-    public slots:
-        void updateGameObjects();
-        void beginFirstTask();
-        void nextTask();
+	public slots:
+		void updateGameObjects();
+		void beginFirstTask();
+		void nextTask();
 
-    signals:
-        void renderGameObjects(std::vector<ObjectRenderInformation> renderables);
-        void startUp();
+	signals:
+		void renderGameObjects(std::vector<ObjectRenderInformation> renderables);
+		void startUp();
 };
 
 #endif // FISHMODEL_H
