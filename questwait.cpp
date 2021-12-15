b46f70345c091675fe2a3d479c22415779e7aa75
ALEXANDER RICHINS <u1208522@umail.utah.edu>
2021-12-14 21:23:25 -0700

    adding basic wait instructions

--- questwait.cpp
+++ questwait.cpp
@@ -28,6 +28,7 @@ void Wait::listener(const CallbackOptions &callback)
 			auto theFish = pleco ? pleco : goldfish ? goldfish : moorish;
 			theFish->setClickable(true);
 		}
+		callback.deleteGameObject("waitinstructions");
 		emit Quest::pass();
 	}
 
@@ -42,5 +43,19 @@ void Wait::reset(const CallbackOptions &callback)
 
 void Wait::showText(const CallbackOptions &callback)
 {
+	Bowl *bowl = (Bowl *) callback.getGameObject("bowl");
+	if (bowl)
+	{
+		GameObject* instructionText = createTextImage("waitinstructions", "The water needs to wait a day to allow the chlorine to evaporate.\nClick the clock to speed up time", QPointF(4, 1), QPointF(15,3), 100);
+		callback.addGameObject(instructionText);
+	}
 
+	auto pleco = (Fish *) callback.getGameObject("pleco");
+	auto goldfish = (Fish *) callback.getGameObject("goldfish");
+	auto moorish = (Fish *) callback.getGameObject("moorish");
+	if (pleco != nullptr || goldfish != nullptr || moorish != nullptr)
+	{
+		GameObject* instructionText = createTextImage("waitinstructions", "The fish needs to have time to adjust to it's new climate slowly over a day.\nCLick the clock to speed up time", QPointF(4, 1), QPointF(15,3), 100);
+		callback.addGameObject(instructionText);
+	}
 }
